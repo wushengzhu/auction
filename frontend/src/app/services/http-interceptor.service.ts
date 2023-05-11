4f292495c92d73a740907ca9ac2eca9206ee8ace
wushengzhu <ws_zhu@foxmail.com>
2023-05-11 14:30:40 +0800

    fix: interceptor

--- frontend/src/app/services/http-interceptor.service.ts
+++ frontend/src/app/services/http-interceptor.service.ts
@@ -36,13 +36,11 @@ export class HttpInterceptorService implements HttpInterceptor {
           }
         },
         (err) => {
-          if (err.status) {
-            this.router.navigate(['/login']);
-          } else {
-            this.mesSvc.error(`请求失败：${err?.status} ${err?.statusText}`);
-          }
-          // this.router.navigate(['/login']);
-          // localStorage.removeItem('token');
+          // if (err.status) {
+          //   this.router.navigate(['/login']);
+          // } else {
+          this.mesSvc.error(`请求失败：${err?.status} ${err?.statusText}`);
+          // }
         }
       )
     );
